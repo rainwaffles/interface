c8cf331142117e8d29f2757e0a1020b4f6e14885
Jeffrey <jchen1352@gmail.com>
2016-02-20 15:37:55 -0800

    basic stuff

--- interface.cpp
+++ interface.cpp
@@ -3,13 +3,33 @@
 .___        __                 _____                            
 |   | _____/  |_  ____________/ ____\____    ____  ____   ______
 |   |/    \   __\/ __ \_  __ \   __\\__  \ _/ ___\/ __ \ /  ___/
-|   |   |  \  | \  ___/|  | \/|  |   / __ \\  \__\  ___/ \___ \ 
+|   |   |  \  | \  ___/|  | \/|  |   / __ \\  \__\  ___/ \___ \
 |___|___|  /__|  \___  >__|   |__|  (____  /\___  >___  >____  >
 
 */
 
+#include <stdio.h>
 
 int main()
 {
-	cin >>
+	FILE* mission_out;  //read from mission
+	FILE* mission_in;   //write to mission
+	char input_message, input_type;
+	char* input_args;
+	bool quit;
+
+	mission_out = fopen("./missionout","r");
+	mission_in = fopen("./missionin", "w");
+	quit = false;
+	while (!quit) {
+		fscanf(mission_out, "%c", &input_message);
+		switch (input_message) {
+			case 'c': //stuff 
+				break;
+			case 'q': //stuff
+				break;
+		}
+	}
+
+	return 0
 }
